10cab68b6066550989ec11c8ce6f6d4331d036d2
Marco Avelar <marco.avelar.cabrera@gmail.com>
2022-02-11 13:22:05 -0600

    added install script for common initial dependecies, added systray diff for dwm

--- autologin.sh
+++ autologin.sh
@@ -4,7 +4,7 @@ if [ ! -d /etc/systemd/system/getty\@tty1.service.d ]; then
     sudo mkdir /etc/systemd/system/getty\@tty1.service.d
 fi
 
-sudo cp ~/dotemacs/autologin.conf /etc/systemd/system/getty\@tty1.service.d/autologin.conf
+sudo cp autologin.conf /etc/systemd/system/getty\@tty1.service.d/autologin.conf
 
-cp ~/dotemacs/.bash_profile ~/.bash_profile
-cp ~/dotemacs/.xinitrc ~/.xinitrc
+cp .bash_profile ~/.bash_profile
+cp .xinitrc ~/.xinitrc
---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+rm -rf output
+mkdir output
+
+cd output
+git clone https://aur.archlinux.org/pikaur.git
+
+cd pikaur
+makepkg -fsri
+
+cd ..
+rm -rf pikaur
+
+sudo pacman -S xorg xorg-xinit emacs autorandr vim pulseaudio pulseaudio-bluetooth pulsemixer scrot xfce4-terminal the_silver_searcher ttf-liberation
+
+pikaur -S brave-bin
+
+git clone https://git.suckless.org/dwm
+
+cd dwm
+git apply ../../dwm/systram.diff
+cp ../../dwm/config.h config.h 
+make clean; make; sudo make install;
+cd ..
+
+git clone https://git.suckless.org/slstatus
+cd slstatus
+cp ../../slstatus/config.h config.h
+make clean; make; sudo make install;
+cd ../../
+
+./autologin.sh
